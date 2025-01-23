2caaae930eabae30521911e9b3bd15b66281765d
Guillaume LEDUC <gduc.37@gmail.com>
2025-01-23 16:29:54 +0100

    Ajout du composant Detail et suppression des composants de graphiques obsolètes, mise à jour des styles et intégration de ngx-charts pour les visualisations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DetailComponent } from './pages/detail/detail.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundComponent, HomeComponent],
-  imports: [BrowserModule, NgxChartsModule, AppRoutingModule, HttpClientModule, DetailComponent, BrowserAnimationsModule, RouterModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgxChartsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   OnInit,
 } from '@angular/core';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Router } from '@angular/router';
 import { filter, map, Observable, take } from 'rxjs';
@@ -15,49 +18,31 @@ interface PieChartData {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  public olympics$!: Observable<Olympic[] | undefined>;
-  pieChartData$!: Observable<PieChartData[]>;
-
-  view: [number, number] = [600, 400];
-
-  private viewWidth!: number;
-  private setViewWidth(): void {
-    if (window.innerWidth < 576) {
-      this.viewWidth = 300;
-    } else if (window.innerWidth >= 576 && window.innerWidth < 768) {
-      this.viewWidth = 500;
-    } else if (window.innerWidth >= 768 && window.innerWidth < 992) {
-      this.viewWidth = 700;
-    } else {
-      this.viewWidth = 700;
-    }
-    this.view = [this.viewWidth, 400];
-  }
+  public olympics?: Olympic[];
+  view : [number, number] = [700, 400];
 
   constructor(
-    private router: Router,
-    private olympicService: OlympicService
+    private olympicService: OlympicService,
+    private cdk: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.olympics$
-    this.pieChartData$ = this.olympics$
-      .pipe(
-        map((olympics) => this.convertOlympicsToPieChartData(olympics)),
-      ) 
-    this.setViewWidth();
-    window.addEventListener('resize', () => {
-      this.setViewWidth();
+    this.olympicService.olympics$.subscribe((olympics) => {
+      if (olympics) this.olympics = olympics;
+      this.cdk.markForCheck();
     });
   }
 
   convertOlympicsToPieChartData(
-    olympics: Olympic[] | undefined
-  ): PieChartData[] {
+    olympics: Olympic[]
+  ): { name: string; value: number }[] {
     const data: { name: string; value: number }[] = [];
-      olympics?.forEach((olympic) => {
+
+    if (this.olympics) {
+      this.olympics.forEach((olympic) => {
         data.push({
           name: olympic.country,
           value: olympic.participations.reduce(
@@ -66,12 +51,16 @@ export class HomeComponent implements OnInit {
           ),
         });
       });
+    }
+
     return data;
   }
 
+  colorScheme : { domain : Array<string> }= {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
   onSelect(data: any): void {
-    const { name } = JSON.parse(JSON.stringify(data));
-    const slug = name.split(' ').join('-').toLowerCase();
-    this.router.navigate([`/details/${slug}`]);
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
   }
 }
