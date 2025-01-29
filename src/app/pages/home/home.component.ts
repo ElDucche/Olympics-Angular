ed872c9e593a00ce6b04020d2bab8f435ba1d597
Guillaume LEDUC <gduc.37@gmail.com>
2025-01-29 18:44:37 +0100

    Refactor le composant Home : mise à jour de la logique de récupération des données pour le graphique en secteurs, utilisation d'un Observable pour les données et amélioration des performances de rendu.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,8 +4,10 @@ import {
 } from '@angular/core';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { Router } from '@angular/router';
-import { filter, map, Observable, take } from 'rxjs';
+import { Router, RouterModule } from '@angular/router';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { map, Observable, take, tap } from 'rxjs';
 
 interface PieChartData {
   name: string;
@@ -15,9 +17,10 @@ interface PieChartData {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  public olympics$!: Observable<Olympic[] | undefined>;
+  public olympics?: Olympic[];
   pieChartData$!: Observable<PieChartData[]>;
 
   view: [number, number] = [600, 400];
@@ -37,24 +40,25 @@ export class HomeComponent implements OnInit {
   }
 
   constructor(
+    // private cdk: ChangeDetectorRef,
     private router: Router,
     private olympicService: OlympicService
   ) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.olympics$
-    this.pieChartData$ = this.olympics$
+    this.pieChartData$ = this.olympicService.olympics$
       .pipe(
         map((olympics) => this.convertOlympicsToPieChartData(olympics)),
-      ) 
+      )
     this.setViewWidth();
     window.addEventListener('resize', () => {
       this.setViewWidth();
+      // this.cdk.markForCheck();
     });
   }
 
   convertOlympicsToPieChartData(
-    olympics: Olympic[] | undefined
+    olympics: Olympic[] | null
   ): PieChartData[] {
     const data: { name: string; value: number }[] = [];
       olympics?.forEach((olympic) => {
@@ -66,8 +70,13 @@ export class HomeComponent implements OnInit {
           ),
         });
       });
+      console.log(data);
     return data;
   }
+
+  colorScheme: { domain: Array<string> } = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
+  };
 
   onSelect(data: any): void {
     const { name } = JSON.parse(JSON.stringify(data));
