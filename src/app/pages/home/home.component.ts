11319b6440d9d9a5e76d619df4b32ba1e66192e8
Guillaume LEDUC <gduc.37@gmail.com>
2025-01-30 09:55:45 +0100

    Refactor le composant Home : suppression des vérifications nulles, amélioration de la gestion des données et nettoyage du code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
-import { Router, RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { map, Observable, take, tap } from 'rxjs';
@@ -19,10 +19,9 @@ interface PieChartData {
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
-  public olympics?: Olympic[];
+  public olympics!: Olympic[];
   pieChartData$!: Observable<PieChartData[]>;
 
   view: [number, number] = [600, 400];
@@ -42,7 +41,6 @@ export class HomeComponent implements OnInit {
   }
 
   constructor(
-    // private cdk: ChangeDetectorRef,
     private router: Router,
     private olympicService: OlympicService
   ) {}
@@ -50,12 +48,14 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.pieChartData$ = this.olympicService.olympics$
       .pipe(
-        map((olympics) => this.convertOlympicsToPieChartData(olympics)),
+        map((olympics) => this.convertOlympicsToPieChartData(olympics))
       )
+    this.olympicService.olympics$.subscribe((olympics) => {
+      this.olympics = olympics || [];
+    });
     this.setViewWidth();
     window.addEventListener('resize', () => {
       this.setViewWidth();
-      // this.cdk.markForCheck();
     });
   }
 
@@ -72,17 +72,11 @@ export class HomeComponent implements OnInit {
           ),
         });
       });
-      console.log(data);
     return data;
   }
 
-  colorScheme: { domain: Array<string> } = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'],
-  };
-
   onSelect(data: any): void {
     const { name } = JSON.parse(JSON.stringify(data));
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
     const slug = name.split(' ').join('-').toLowerCase();
     this.router.navigate([`/details/${slug}`]);
   }
