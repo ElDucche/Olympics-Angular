e7ea579b267a9b473e010d2be2f4cbe8343ee9b0
ElDucche <gduc.37@gmail.com>
2025-02-14 11:16:34 +0100

    Refactor le composant Detail : amélioration de la gestion des données avec des Observables, suppression de la logique obsolète et nettoyage du code

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, OutletContext, RouterModule } from '@angular/router';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { Subscription } from 'rxjs';
-=======
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
@@ -14,38 +8,35 @@ import {
 import { ActivatedRoute, OutletContext, RouterModule } from '@angular/router';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { map, Observable, Subscription, take } from 'rxjs';
->>>>>>> 2fc57d7 (Refactor le composant Home et Detail : mise à jour de la gestion des données avec des Observables, amélioration de la logique de récupération des données et ajout de la gestion des états de chargement)
 import { Olympic } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+
+interface LineChartDatas {
+  name: string;
+  series: { name: string; value: number }[];
+}
 
 @Component({
   selector: 'app-detail',
   standalone: true,
   templateUrl: './detail.component.html',
-<<<<<<< HEAD
-  imports: [NgxChartsModule, RouterModule]
-=======
   imports: [NgxChartsModule, RouterModule],
   // changeDetection: ChangeDetectionStrategy.OnPush,
->>>>>>> 2fc57d7 (Refactor le composant Home et Detail : mise à jour de la gestion des données avec des Observables, amélioration de la logique de récupération des données et ajout de la gestion des états de chargement)
 })
-export class DetailComponent implements OnInit, OnDestroy {
-  private subscription!: Subscription;
+export class DetailComponent implements OnInit {
   public countryName: string;
-<<<<<<< HEAD
-  public olympicData: any;
-=======
   olympic$!: Observable<Olympic | undefined>;
   lineChartData$!: Observable<LineChartDatas[] | undefined>;
   totalMedals$!: Observable<number>;
   totalAthletes$!: Observable<number>;
 
   // Transformer tous mes appels aux méthodes en observable pour pipe | async
->>>>>>> 2fc57d7 (Refactor le composant Home et Detail : mise à jour de la gestion des données avec des Observables, amélioration de la logique de récupération des données et ajout de la gestion des états de chargement)
 
   view: [number, number] = [600, 400];
 
   private viewWidth!: number;
+
   private setViewWidth(): void {
     if (window.innerWidth < 576) {
       this.viewWidth = 300;
@@ -59,28 +50,17 @@ export class DetailComponent implements OnInit, OnDestroy {
     this.view = [this.viewWidth, 400];
   }
 
-<<<<<<< HEAD
-  constructor(route: ActivatedRoute, private olympic$: OlympicService, private cdk: ChangeDetectorRef) {
-=======
   constructor(
     route: ActivatedRoute,
     private olympicService: OlympicService
   ) // private cdk: ChangeDetectorRef
   {
->>>>>>> 2fc57d7 (Refactor le composant Home et Detail : mise à jour de la gestion des données avec des Observables, amélioration de la logique de récupération des données et ajout de la gestion des états de chargement)
     this.countryName = String(route.snapshot.params['name']).toLowerCase();
   }
 
   ngOnInit(): void {
+    console.log('Initialisation du composant');
     const formattedName = this.countryName.split('-').join(' ');
-<<<<<<< HEAD
-    console.log('Country name = ' + formattedName);
-    this.subscription = this.olympic$.getOlympicByCountry(formattedName).subscribe((olympic) => {
-      this.olympicData = olympic;
-      console.log(this.olympicData);
-    });
-    this.cdk.markForCheck();
-=======
     this.olympic$ = this.olympicService.getOlympicByCountry(formattedName);
     this.lineChartData$ = this.olympic$.pipe(
       map((olympic) => this.convertOlympicDataToLineChartData(olympic))
@@ -106,7 +86,6 @@ export class DetailComponent implements OnInit, OnDestroy {
       })
     );
     // this.cdk.markForCheck();
->>>>>>> 2fc57d7 (Refactor le composant Home et Detail : mise à jour de la gestion des données avec des Observables, amélioration de la logique de récupération des données et ajout de la gestion des états de chargement)
     this.setViewWidth();
     window.addEventListener('resize', () => {
       this.setViewWidth();
@@ -114,44 +93,6 @@ export class DetailComponent implements OnInit, OnDestroy {
     });
   }
 
-<<<<<<< HEAD
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
-
-  getTotalMedals(): number {
-    return (
-      this.olympicData?.participations?.reduce(
-        (acc: number, curr: any) => acc + curr.medalsCount,
-        0
-      ) || 0
-    );
-  }
-
-  getTotalNumberOfAthletes(): number {
-    return (
-      this.olympicData?.participations?.reduce(
-        (acc: number, curr: any) => acc + curr.athleteCount,
-        0
-      ) || 0
-    );
-  }
-
-  convertOlympicDataToLineChartData(olympicData: Olympic) : { name:string; series: {name: string; value: number}[]}[] {
-    return [{
-      name: olympicData.country,
-      series: olympicData.participations.map((participation) => {
-        return {
-          name: String(participation.year),
-          value: participation.medalsCount
-        }
-      })}
-    ]
-  }
-}
-=======
   convertOlympicDataToLineChartData(
     olympicData: Olympic | undefined
   ): { name: string; series: { name: string; value: number }[] }[] {
@@ -169,4 +110,3 @@ export class DetailComponent implements OnInit, OnDestroy {
     ];
   }
 }
->>>>>>> 2fc57d7 (Refactor le composant Home et Detail : mise à jour de la gestion des données avec des Observables, amélioration de la logique de récupération des données et ajout de la gestion des états de chargement)
